ba07842003b9c6e67cd24719f4a125ade0bbaf05
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-10-02 21:48:32 +0300

    Add project files.

---
+++ DAL.UrunYonetimi/Context/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UrunYonetimi.Context
+{
+    public class ProductDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-3HH2461E;Database=ANK18ProductDb;Trusted_Connection=true;Trustservercertificate=true");
+
+        }
+    }
+}
---
+++ DAL.UrunYonetimi/Entities/Abstract/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using DAL.UrunYonetimi.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UrunYonetimi.Entities.Abstract
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime? ModifiedDate { get; set; }
+        DateTime? DeletedDate { get; set; }
+        DataStatus DataStatus { get; set; }
+
+
+    }
+}
